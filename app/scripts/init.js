4fd9f7657a67a1c6b7faacb42c0be0666a27665c
Fengda HUANG <h@phodal.com>
2015-02-21 09:15:35 +0800

    remove jquery

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -1,16 +1,9 @@
 require.config({
-  baseUrl: 'app',
-  paths: {
-    jquery: 'lib/jquery'
-  }
+  baseUrl: 'app'
 });
 
-require(['lib/knockout', 'scripts/TalentTree', 'scripts/TalentData', 'jquery'], function(ko, TalentTree, TalentData, $) {
+require(['lib/knockout', 'scripts/TalentTree', 'scripts/TalentData'], function(ko, TalentTree, TalentData) {
   'use strict';
   var vm = new TalentTree(TalentData);
   ko.applyBindings(vm);
-
-  setTimeout(function () {
-    $('.page').addClass('animated');
-  }, 50);
 });
