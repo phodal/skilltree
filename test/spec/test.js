ca2c34a6878789215be66601ca84c76f2cfa4f03
Fengda HUANG <h@phodal.com>
2015-02-22 10:13:20 +0800

    add jquery map

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,13 +1,13 @@
 /* global describe, it */
 
-define(function(require) {
+define(function (require) {
   'use strict';
 
   describe('Give it some context', function () {
     describe('maybe a bit more context here', function () {
       it('should run here few assertions', function () {
-
+        
       });
     });
   });
-})();
+});
