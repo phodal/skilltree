16ce88d21b52c0831c1b91ae3d025d1bc16df77f
Fengda HUANG <h@phodal.com>
2015-02-23 11:33:22 +0800

    change reporter to spec

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,9 @@ var mocha = require('gulp-mocha');
 
 gulp.task('test', function () {
   return gulp.src('test/spec/*_spec.js', {read: false})
-    .pipe(mocha({reporter: 'nyan'}));
+    .pipe(mocha({
+        reporter: 'spec'
+      }));
 });
 
 gulp.task('jshint', function () {
