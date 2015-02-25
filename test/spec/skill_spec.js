3a6c24102a66891a7431acc7b2a1d4ce735b67d8
Fengda HUANG <h@phodal.com>
2015-02-25 10:09:49 +0800

    use web.json to test data

--- test/spec/skill_spec.js
+++ test/spec/skill_spec.js
@@ -3,55 +3,22 @@
 var requirejs = require("requirejs");
 var assert = require("assert");
 var should = require("should");
+var skill = require('fs').readFileSync('./app/data/web.json', 'utf8');
+
 requirejs.config({
   baseUrl: 'app/',
   nodeRequire: require
 });
 
 describe('Skill', function () {
-  var Skill, _skill;
+  var Skill, html_skill;
   before(function (done) {
     requirejs(['scripts/Skill'], function (Skill_Class) {
       Skill = Skill_Class;
       done();
     });
   });
-  var html_skill = {
-    "id": 1,
-    "title": "HTML",
-    "description": "Internet主要由从服务器通过HTTP协议向浏览器发送的HTML文档组成。HTML被用来结构化信息——例如标题、段落和列表等等，也可用来在一定程度上描述文档的外观和语义。",
-    "rankDescriptions": [
-      "理解如何创建和浏览一个基本的网页",
-      "理解网页间是如何链接的、如何设计多列布局，可以处理表单字段和媒体元素"
-    ],
-    "links": [
-      {
-        "label": "无处不在的html",
-        "url": "http://www.phodal.com/blog/be-a-geek-chapter-1-anywhere-html/"
-      }
-    ],
-    "books": [
-      {
-        "label": "Head First HTML与CSS",
-        "url": "http://www.phodal.com"
-      }
-    ],
-    "maxPoints": 2,
-    "stats": [
-      {
-        "title": "Intellect",
-        "value": 1
-      },
-      {
-        "title": "Charisma",
-        "value": 1
-      },
-      {
-        "title": "Strength",
-        "value": 1
-      }
-    ]
-  };
+  html_skill = JSON.parse(skill).skills[0];
 
   describe('Parse Test', function () {
     it('should parse skill tree', function () {
@@ -65,13 +32,13 @@ describe('Skill', function () {
       _skill.canRemovePoints().should.equal(false);
       _skill.talentSummary().should.equal('');
     });
-    
+
     it('should parse books & links', function () {
       var _skill = new Skill(html_skill);
       _skill.links[0].label.should.equal('无处不在的html');
       _skill.links[0].url.should.equal('http://www.phodal.com/blog/be-a-geek-chapter-1-anywhere-html/');
       _skill.books[0].label.should.equal('Head First HTML与CSS');
-      _skill.books[0].url.should.equal('http://www.phodal.com');
+      _skill.books[0].url.should.equal('http://www.amazon.cn/Head-First-HTML%E4%B8%8ECSS-%E7%BD%97%E5%B8%83%E6%A3%AE/dp/B00FF3P8FY/ref=sr_1_1?ie=UTF8&qid=1424182950&sr=8-1&keywords=html');
     });
   });
 
