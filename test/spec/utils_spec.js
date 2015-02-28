3826c0c60d471113f5c177931b4201e8d6e16cb6
Fengda HUANG <h@phodal.com>
2015-02-28 09:22:01 +0800

    add test for get skill by id

--- test/spec/utils_spec.js
+++ test/spec/utils_spec.js
@@ -4,6 +4,7 @@ var requirejs = require("requirejs");
 var assert = require("assert");
 var should = require("should");
 var jsdom = require('mocha-jsdom');
+var skill = require('fs').readFileSync('./app/data/web.json', 'utf8');
 
 requirejs.config({
   baseUrl: 'app/',
@@ -11,19 +12,34 @@ requirejs.config({
 });
 
 describe('Utils', function () {
-  var Utils;
+  var Utils, TalentTree, Skill, ko;
   jsdom();
   before(function (done) {
-    requirejs(['scripts/Utils', 'scripts/Link'], function (Utils_Class) {
-      Utils = Utils_Class;
-      done();
-    });
+    requirejs(['scripts/Utils', 'scripts/Skill', 'lib/knockout'],
+      function (Utils_Class, Skill_Class, Knockout) {
+        Utils = Utils_Class;
+        Skill = Skill_Class;
+        ko = Knockout;
+        done();
+      });
   });
+  var all_skills = JSON.parse(skill);
 
-  describe('Utils', function () {
+  describe('Pretty Join', function () {
     it('should return string with and when use array', function () {
       var dependencies = ["Web Design Mastery", "jQuery Effects"];
       Utils.prettyJoin(dependencies).should.equal("Web Design Mastery 和 jQuery Effects")
     });
   });
+
+  describe('Get Skill by ID', function () {
+    it('should return string with and when use array', function () {
+      var skills = ko.observableArray(ko.utils.arrayMap(all_skills.skills, function (item) {
+        return new Skill(item);
+      }));
+      Utils.getSkillById(skills(), 1).id.should.equal(1);
+      Utils.getSkillById(skills(), 1).title.should.equal('HTML');
+    });
+  });
+
 });
