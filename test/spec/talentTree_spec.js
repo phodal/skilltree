1f00b44f0b2fee3deb6c49b6dd60396fa1707b04
Fengda HUANG <h@phodal.com>
2015-02-28 08:58:55 +0800

    Add test for stats

--- test/spec/talentTree_spec.js
+++ test/spec/talentTree_spec.js
@@ -67,5 +67,33 @@ describe('Talent Tree Test', function () {
         talent.talentSummary().should.equal("时尚, 咝咝, 狡猾, 灵活, 美型男, XXL 背包, 真理探寻者, 心灵编织, 思想穿透者, 艺术家, 咒术师, 炼金, 监管者, 频谱指南, 半仙");
       });
     });
+
+    describe('Stats Test', function () {
+      it('should return correctly stats 1', function () {
+        var talent = new TalentTree(all_skills);
+        talent.updateLastHash("_m2n2x2y2_1_Phodal");
+        talent.stats().toString().should.equal([
+          { title: '魅力', value: 9 },
+          { title: '灵巧', value: 13 },
+          { title: '坚韧', value: 19 },
+          { title: '智力', value: 9 },
+          { title: '力量', value: 19 },
+          { title: '智慧', value: 9 }
+        ].toString());
+      });
+
+      it('should return correctly stats 2', function () {
+        var talent = new TalentTree(all_skills);
+        talent.updateLastHash("_a2b2c2de3fgh3i2jklm2n2opqr2s2tuvwx2y2z_2_Name");
+        talent.stats().toString().should.equal([
+          { title: '魅力', value: 53 },
+          { title: '灵巧', value: 45 },
+          { title: '坚韧', value: 27 },
+          { title: '智力', value: 21 },
+          { title: '力量', value: 40 },
+          { title: '智慧', value: 26 }
+        ].toString());
+      });
+    });
   });
 });
